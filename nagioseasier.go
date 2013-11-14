8d2868b5010c169f1dbc816eef5df7fdc0cdf229
Will Farrington <wfarr@github.com>
2013-11-14 01:20:25 -0800

    basic r/w

--- nagioseasier.go
+++ nagioseasier.go
@@ -2,35 +2,80 @@ package main
 
 import (
 	"fmt"
-	"io"
+//	"io"
+	"os"
 	"net"
+	"sort"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("unix", "/var/lib/nagios/rw/nagios.qh")
 
+	// establish connection to our socket, for both reads and writes
+	conn, err := net.Dial("unix", "/var/lib/nagios/rw/nagios.qh")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	// suss out what command we actually wish to run
+	if len(os.Args) == 0 {
+		send_command(conn, "help")
+	}
+
+	switch os.Args[1] {
+	case "help", "status", "acknowledge", "unacknowledge", "disable_notifications", "enable_notifications", "downtime", "problems":
+		send_command(conn, strings.Join(os.Args[1:], " "))
+	case "ack":
+		send_command(conn, "acknowledge" + strings.Join(os.Args[2:], " "))
+	case "unack":
+		send_command(conn, "unacknowledge" + strings.Join(os.Args[2:], " "))
+	case "mute":
+		send_command(conn, "disable_notifications" + strings.Join(os.Args[2:], " "))
+	case "unmute":
+		send_command(conn, "enable_notifications" + strings.Join(os.Args[2:], " "))
+	default:
+		send_command(conn, "help")
+	}
+
+	output := read_results(conn)
+
+	lines := strings.Split(output, "\n")
+	sort.Sort(sort.StringSlice(lines))
+
+	for _, line := range lines[1:] {
+
+		if strings.Count(line, ";") == 3 {
+			fmt.Println(line)
+		} else {
+			// whatever
+			fmt.Println(line)
+		}
+	}
+}
+
+func send_command(conn net.Conn, cmd string) {
+	_, err := conn.Write([]byte(fmt.Sprintf("#nagioseasier %s\000", cmd)))
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer conn.Close()
+}
 
-	go func(r io.Reader) {
-		buf := make([]byte, 4096)
+func read_results(conn net.Conn) (output string) {
+	buf := make([]byte, 4096)
+	for ;; {
+		n, err := conn.Read(buf[:])
 
-		for {
-			n, err := r.Read(buf[:])
-			if err != nil {
-				return
-			}
-
-			fmt.Println("got:", string(buf[0:n]))
+		if err != nil {
+			return output
 		}
-	} (conn)
 
-	_, err = conn.Write([]byte("#help"))
+		if n == 0 {
+			fmt.Println("Connection closed by socket")
+			return output
+		}
 
-	if err != nil {
-		panic(err.Error())
+		output = output + string(buf[0:n])
 	}
 }
